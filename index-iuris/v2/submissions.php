c80fc933dd7151e81d2e6ce0218bff7fe99efdd9
adrq <Adrian@Jupiter.local>
2015-07-24 14:00:07 -0400

    updated submissions page

--- index-iuris/v2/submissions.php
+++ index-iuris/v2/submissions.php
@@ -20,20 +20,28 @@ require "includes/header.php";
 	}
 	
 	
-	$statement = $mysqli->prepare("SELECT (id, url, title, archive, type) FROM objects WHERE user_id = ?");
-	$statement->bind_param("s", $_POST['user_id']);
+	$statement = $mysqli->prepare("SELECT id, url, title, archive, type FROM objects WHERE user_id = ?");
+	$statement->bind_param("s", $_SESSION['user_id']);
 	$statement->execute();
 	$statement->store_result();
 	$statement->bind_result($id, $url, $title, $archive, $type);
 	
 	if ($statement->num_rows > 0){
-		while ($statement->fetch()){
+		while ($statement->fetch()){ //TODO: add some sort of pagination if there are many submisions per user
 			?>
-			<div class="submission">
-				
+			<div class="submission" style="border-bottom: 1px solid black;">
+				<h3><?php print $title?></h3>
+				<p><strong>URL: </strong><?php print $url?></p>
+				<p><strong>Archive: </strong><?php print $url?></p>
+				<p><strong>Type: </strong><?php print $url?></p>
+				<p><strong>Object ID: </strong><?php print $url?></p>
+				<a href="#">View object</a> - <a href="#">Edit object</a>
 			</div>
 			<?php 
 		}
+	}
+	else {
+		print "You have not made any submissions yet.";
 	}
 	
 	
