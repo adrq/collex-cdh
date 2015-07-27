b7eb9b59d2134c371520ed300750797381e1b1b4
Collin Haines <collinhaines@me.com>
2015-07-27 15:02:58 -0400

    Edit layout updated

--- index-iuris/v2/edit.php
+++ index-iuris/v2/edit.php
@@ -1,97 +1,89 @@
 <?php
 /**
- * @file index.php
- * edit submission
+ * @file edit.php
+ * Prints the edit submission page.
  */
-$title = "Edit submission";
+if (isset($_GET["custom_namespace"], $_GET["archive"], $_GET["type"])) {
+  // Do stuff.
+}
+
+$title = "Edit Submission";
 $loginRequired = true;
-
 require "includes/header.php";
-?>
 
-<div class="container">
-  <div class="row">
-	
-	<?php 
-	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_BASE);
-	
-	if ($mysqli->connect_error) {
-		exit("<h2 class='text-danger'>Database connection error. (" . $mysqli->connect_errno . ")</h2>");
-	}
-	
-	//print $_GET['id'];
-	$statement = $mysqli->prepare("SELECT custom_namespace, rdf_about, archive, title, type, url, origin, provenance, place_of_composition, shelfmark, freeculture, full_text_url, full_text_plain, is_full_text, image_url, source, metadata_xml_url, metadata_html_url, text_divisions, language, ocr, thumbnail_url, notes, file_format, date_created, date_updated, user_id FROM objects WHERE id = ? LIMIT 1");
-	$statement->bind_param("s", $_GET['id']);
-	$statement->execute();
-	
-	if ($result = $statement->get_result()){
-		
-		if ($row = $result->fetch_assoc()){
-			if($row['user_id'] == $_SESSION['user_id']){
-				?>
-				<div class="row">
-					<div class="col-xs-12">
-						<?php foreach ($row as $key=>$value): 
-						$type = $objectsTableInputTypes[$key];
-						switch ($type){
-							case "text": ?>
-								<div class="form-group">
-          					      <label for="<?php print $key?>" class="control-label col-xs-2"><?php print $objectsTableColumDisplayNames[$key]?></label>
-             					   <div>
-            				      	<input type="text" class="form-control" name="<?php print $key?>" id="<?php print $key?>" value="<?php print $value?>">
-        					       </div>
-           						</div><hr>
-							
-							<?php 
-							break;
-							case "textarea": ?>
-								<div class="form-group">
-          					      <label for="<?php print $key?>" class="control-label col-xs-2"><?php print $objectsTableColumDisplayNames[$key]?></label>
-             					   <div>
-            				      		<textarea class="form-control" name="<?php print $key?>" id="<?php print $key?> rows="4"></textarea>
-        					       </div>
-           						</div><hr>
-							<?php 
-							break;
-							case "radio": ?>
-								<div class="form-group">
-           					    	<label class="control-label"><?php print $objectsTableColumDisplayNames[$key]?></label>
-             						<div>
-              					    	<div class="radio">
-              					        	<label><input type="radio" name="<?php print $key?>" value="true" <?php if ($value == "true") print "checked=\"true\"";?>>Yes</label>
-              					    	</div>
-              					    	<div class="radio">
-             					    		<label><input type="radio" name="<?php print $key?>" value="false" <?php if ($value == "false") print "checked=\"true\"";?>>No</label>
-              					    	</div>
-               					 	</div>
-             					 </div><hr>
-							<?php 
-						}
-						
-						?>
-						
-						<?php endforeach; ?>
-					</div>
-				</div>
-				<?php 
-			}
-			else {?>
-				<script>alert("You do not have permission to view this record."); window.location = "submissions";</script>
-			<?php }			
-		}
-		else {
-			?><script>alert("This record does not exist."); window.location = "submissions";</script><?php
-		}
-	}
-	else {
-		?><script>alert("This record does not exist."); window.location = "submissions";</script><?php
-	}
-	
-	
-	
-	?>
-	
-  </div>
-</div>
+if (!isset($_GET["id"])):
+  ?><script>window.location = "submissions";</script><?php
+else:
+  global $mysqli;
 
-<?php require "includes/footer.php"; ?>
+  $id = $_GET["id"];
+
+  $statement = $mysqli->prepare("SELECT custom_namespace, rdf_about, archive, title, type, url, origin, provenance, place_of_composition, shelfmark, freeculture, full_text_url, full_text_plain, is_full_text, image_url, source, metadata_xml_url, metadata_html_url, text_divisions, language, ocr, thumbnail_url, notes, file_format, date_created, date_updated, user_id FROM objects WHERE id = ? LIMIT 1");
+  $statement->bind_param("s", $id);
+  $statement->execute();
+
+  $row = $statement->get_result()->fetch_assoc();
+
+  if ($row):
+    if ($row["user_id"] == $_SESSION["user_id"]):
+      ?>
+      <div class="container">
+        <div class="row page-header">
+          <div class="col-xs-12">
+            <h1 class="pull-left">Edit <?php print $row["title"]; ?></h1>
+            <p class="last-updated">Last Updated: <time><?php print $row["date_updated"]; ?></time></p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-12">
+            <form class="form-horizontal" action="<?php print htmlentities($_SERVER['PHP_SELF']); ?>" method="GET">
+              <fieldset>
+                <?php foreach ($row as $key=>$value): ?>
+                  <?php switch ($objectsTableInputTypes[$key]): case "text": // Need a reason as to why PHP can be stupid? Any trailing whitespace between switch and the first case throws an error. http://php.net/manual/en/control-structures.alternative-syntax.php ?>
+                    <section class="form-group">
+                      <label for="<?php print $key; ?>" class="control-label"><?php print $objectsTableColumDisplayNames[$key]; ?></label>
+                      <input type="text" class="form-control" name="<?php print $key; ?>" id="<?php print $key; ?>" value="<?php print $value; ?>">
+                    </section>
+                    <hr>
+                    <?php break; ?>
+                    <?php case "textarea": ?>
+                    <section class="form-group">
+                      <label for="<?php print $key; ?>" class="control-label"><?php print $objectsTableColumDisplayNames[$key]; ?></label>
+                      <textarea class="form-control" name="<?php print $key ;?>" id="<?php print $key; ?>" rows="4"><?php print $value; ?></textarea>
+                    </section>
+                    <hr>
+                    <?php break; ?>
+                    <?php case "radio": ?>
+                    <section class="form-group">
+                      <label for="<?php print $key; ?>" class="control-label"><?php print $objectsTableColumDisplayNames[$key]; ?></label>
+                      <div class="radio">
+                        <label><input type="radio" name="<?php print $key; ?>" value="true" <?php print $value == "true" ? "checked=''" : ""; ?> >Yes</label>
+                      </div>
+                      <div class="radio">
+                        <label><input type="radio" name="<?php print $key; ?>" value="false" <?php print $value == "false" ? "checked=''" : ""; ?> >No</label>
+                      </div>
+                    </section>
+                    <hr>
+                    <?php break; ?>
+                  <?php endswitch; ?>
+                <?php endforeach; ?>
+                <section class="form-group" style="margin-bottom: 15%">
+                  <input type="hidden" class="hide" name="id" value="<?php print $id; ?>">
+                  <button type="submit" class="btn btn-success">Save Changes</button>
+                </section>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    <?php
+    else:
+      ?><script>alert("You do not have permission to view this record."); window.location = "submissions";</script><?php
+    endif; // if ($row["user_id"] == $_SESSION["user_id"])
+  else:
+    ?><script>alert("This record does not exist."); window.location = "submissions";</script><?php
+  endif; // if ($row)
+endif; // if (!isset($_GET["id"]))
+
+require "includes/footer.php"; ?>
