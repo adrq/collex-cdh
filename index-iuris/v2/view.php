f3b8301b6db7de6e911232b31b819d0e280240e3
Collin Haines <collinhaines@me.com>
2015-07-27 11:59:17 -0400

    View layout update

--- index-iuris/v2/view.php
+++ index-iuris/v2/view.php
@@ -18,44 +18,13 @@ else:
 
   $row = $statement->get_result()->fetch_assoc();
 
-  global $objectsTableColumDisplayNames;
-
-  $objectsTableColumDisplayNames = array(
-    "custom_namespace" => "Custom namespace",
-    "rdf_about" => "Unique identifier (URI)",
-    "archive" => "Archive",
-    "title" => "Title",
-    "type" => "Type" ,
-    "url" => "URL" ,
-    "origin" => "Origin" ,
-    "provenance" => "Provenance" ,
-    "place_of_composition" => "Place of Composition" ,
-    "shelfmark" => "Shelfmark" ,
-    "freeculture" => "Freeculture" ,
-    "full_text_url" => "Full text URL" ,
-    "full_text_plain" => "Full text" ,
-    "is_full_text" => "Fulltext" ,
-    "image_url" => "Image URL" ,
-    "source" => "Source" ,
-    "metadata_xml_url" => "XML Metadata URL" ,
-    "metadata_html_url" => "HTML Metadata URL" ,
-    "text_divisions" => "Divisions of the Text" ,
-    "language" => "Language" ,
-    "ocr" => "OCR" ,
-    "thumbnail_url" => "Thumbnail URL" ,
-    "notes" => "Notes" ,
-    "file_format" => "File format" ,
-    "date_created" => "Date created",
-    "date_updated" => "Date updated",
-    "user_id" => "User ID"
-  );
-
   if ($row) {
     ?>
     <div class="container">
       <div class="row page-header">
         <div class="col-xs-12">
-          <h1><?php print $row["title"]; ?></h1>
+          <h1 class="pull-left"><?php print $row["title"]; ?></h1>
+          <a href="#" class="btn btn-default pull-right" style="margin-top: 20px;">Edit</a>
         </div>
       </div>
 
@@ -65,13 +34,45 @@ else:
           <h5><?php print $row["archive"]; ?></h5>
         </div>
 
-        <div class="col-xs-3 pull-right text-right">
+        <div class="col-xs-3 text-right">
           <p>Created: <time><?php print $row["date_created"]; ?></time></p>
           <p>Updated: <time><?php print $row["date_updated"]; ?></time></p>
         </div>
       </div>
 
       <hr>
+
+      <div class="row">
+        <div class="col-xs-4">
+          <ul class="list-group">
+            <?php
+            print printListItem("Language", $row["language"]);
+            print printListItem("Origin", $row["origin"]);
+            print printListItem("Provenance", $row["provenance"]);
+            print printListItem("Place of Composition", $row["place_of_composition"]);
+            print printListItem("File Format", $row["file_format"]);
+            ?>
+          </ul>
+        </div>
+
+        <div class="col-xs-5">
+          <?php
+          print printPanel("Shelfmark", $row["shelfmark"]);
+          print printPanel("Divisions of the Text", $row["text_divisions"]);
+          print printPanel("Source", $row["source"]);
+          print printPanel("Notes", $row["notes"]);
+          ?>
+        </div>
+
+        <div class="col-xs-3">
+          <?php
+          print printWell("Image URL", $row["image_url"]);
+          print printWell("Full Text URL", $row["full_text_url"]);
+          print printWell("HTML Metadata URL", $row["metadata_html_url"]);
+          print printWell("XML Metadata URL", $row["metadata_xml_url"]);
+          ?>
+        </div>
+      </div>
 
 
       <div class="row">
@@ -84,61 +85,52 @@ else:
     </div>
     <?php
   } else {
-    ?><script>alert("This record does not exist.");window.location = "submissions";</script><?php
+    ?><script>alert("This record does not exist."); window.location = "submissions";</script><?php
   }
 
-endif;/*
-?>
-<div class="container">
-  <div class="row page-header">
-    <div class="col-xs-12">
-      <h1>Individual Submission</h1>
-      <p class="lead">ID: <?php print $_GET["id"]; ?></p>
-    </div>
-  </div>
+endif;
 
-  <div class="row">
+require "includes/footer.php";
 
-  <?php
-  $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_BASE);
-
-  if ($mysqli->connect_error) {
-    exit("<h2 class='text-danger'>Database connection error. (" . $mysqli->connect_errno . ")</h2>");
-  }
-
-  //print $_GET['id'];
-  $statement = $mysqli->prepare("SELECT custom_namespace, rdf_about, archive, title, type, url, origin, provenance, place_of_composition, shelfmark, freeculture, full_text_url, full_text_plain, is_full_text, image_url, source, metadata_xml_url, metadata_html_url, text_divisions, language, ocr, thumbnail_url, notes, file_format, date_created, date_updated, user_id FROM objects WHERE id = ? LIMIT 1");
-  $statement->bind_param("s", $_GET['id']);
-  $statement->execute();
-
-  if ($result = $statement->get_result()){
-
-    if ($row = $result->fetch_assoc()){
-
-      if($row['user_id'] == $_SESSION['user_id']){
-        foreach ($row as $key => $value){
-          ?>
-          <p><?php print $key?>: <?php print $value?></p>
-          <?php
-        }
-      }
-      else {
-        print "You do not have permission to view this record.";
-      }
-    }
-    else {
-      print "Unable to find record.";
-    }
-  }
-  else {
-    print "Unable to find record.";
-  }
-
-
-
+/**
+ * Prints a list item.
+ *
+ * @param {String} $text: The heading.
+ * @param {String} $column: The string of an array representing the value of a column.
+ */
+function printListItem($text, $column) {
   ?>
+  <li class="list-group-item"><strong><?php print $text; ?></strong>: <?php print $column === "" ? "<em>Blank</em>" : $column; ?></li>
+  <?php
+}
 
+/**
+ * Prints a panel.
+ *
+ * @param {String} $text: The heading.
+ * @param {String} $column: The string of an array representing the value of a column.
+ */
+function printPanel($text, $column) {
+  ?>
+  <div class="panel panel-default">
+    <div class="panel-heading"><?php print $text; ?></div>
+    <div class="panel-body"><?php print $column; ?></div>
   </div>
-</div>
+  <?php
+}
+/**
+ * Prints a well with an anchor tag.
+ *
+ * @param {String} $text: The heading.
+ * @param {String} $column: The string of an array representing the value of a column.
+ */
+function printWell($text, $column) {
+  ?>
+  <p><?php print $text; ?>:</p>
+  <div class="well well-sm">
+    <a href="<?php print $column; ?>" target="_blank"><?php print $column; ?></a>
+  </div>
+  <?php
+}
 
-<?php */ require "includes/footer.php"; ?>
+
