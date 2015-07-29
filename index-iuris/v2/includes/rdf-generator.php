153407d4dfe5ebfef6f474803026942a1d2e3bdd
Collin Haines <collinhaines@me.com>
2015-07-29 16:13:18 -0400

    Refactored rdf-generator. Added TODO file.

--- index-iuris/v2/includes/rdf-generator.php
+++ index-iuris/v2/includes/rdf-generator.php
@@ -1,43 +1,53 @@
 <?php
-function generateRDF($submission){
-	//mb_internal_encoding("UTF-8");
-	$rdfNamespaces = [
-		'archive' => 'collex:archive',
-		'title' => 'dc:title',
-		'type' => 'dc:type', // need to create new namespace
-		'genre' => 'collex:genre', //need to create new namespace
-		'origin' => 'origin', //need to create new namespace
-		'provenance' => 'provenance', //need to create new namespace
-		'place-of-composition' => 'placeOfComposition', //need to create new namespace
-		'shelfmark' => 'shelfmark', //need to create new namespace
-		'alternative-title' => 'dcterms:alternative',
-		'source' => 'collex:source',
-		'text-divisions' => 'textDivisions', //need to create new namespace
-		'notes' => 'notes', //need to create new namepace
-		'file-format' => 'fileFormat' //need to create new namespace
-				];
-	echo '<br><br>';
-	
-	$rdfHeader = file_get_contents('rdf-header.rdf');
-	
-	$rdf = $rdfHeader."\n<".$submission['custom-namespace']." rdf:about=\"".$submission['rdf:about']."\">\n";
-	$rdf = $rdf."<rdfs:seeAlso rdf:resource=\"".$submission['seeAlso']."\"/>\n";
-	
-	//echo htmlspecialchars($rdf);
-	
-	$rdfContent = "";
-	
-	foreach ($rdfNamespaces as $key => $value){
-		//if (!array_key_exists($key, $submission));
-		//$tmpKey = (string)$key;
-		if ($submission[$key] != '') {
-			$rdfContent = $rdfContent."<".$value.">".$submission[$key]."</".$value.">\n";
-		}
-	}
-	$rdf = $rdf.$rdfContent;
-	$rdf = $rdf."</".$submission['custom-namespace'].">\n</rdf:RDF>\n";
-	//echo '<pre>'.htmlspecialchars($rdf).'</pre>';
-	return $rdf;
-}
+/**
+ * @file rdf-generator.php
+ * Small summary of the current page's existence should go here.
+ */
 
-?>
+/**
+ * Generates an RDF.
+ *
+ * @param {Array} $submission: Brief explanation of this variable goes here.
+ * @return {String}
+ */
+function generateRDF($submission) {
+  $rdfNamespaces = array(
+    "archive" => "collex:archive",
+    "title" => "dc:title",
+    "type" => "dc:type",
+    "genre" => "collex:genre",
+    "origin" => "origin",
+    "provenance" => "placeOfComposition",
+    "shelfmark" => "shelfmark",
+    "alternative-title" => "dcterms:alternative",
+    "source" => "collex:source",
+    "text-divisions" => "textDivisions",
+    "notes" => "notes",
+    "file-format", "fileFormat"
+  );
+
+  print "<br><br>";
+
+  $rdf = "";
+  if (file_exists("rdf-header.rdf")) {
+    $rdf .= file_get_contents("rdf-header.rdf") . "\n<" . $submission["custom-namespace"] . " rdf:about=\"" . $submission["rdf:about"] . "\">\n";
+    $rdf .= "<rdfs:seeAlso rdf:resource=\"" . $submission["seeAlso"] . "\"/>\n";
+
+    // print htmlspecialchars($rdf);
+  }
+
+  foreach ($rdfNamespaces as $key=>$value) {
+    // if (!array_key_exists($key, $submission));
+    // $tempKey = (string) $key;
+
+    if ($submission[$key] !== "") {
+      $rdf .= "<" . $value . ">" . $submission[$key] . "</" . $value . ">\n";
+    }
+  }
+
+  $rdf .= "</" . $submission["custom-namespace"] . ">\n</rdf:RDF>\n";
+
+  // print "<pre>" . htmlspecialchars($rdf) . "</pre>";
+
+  return $rdf;
+}
