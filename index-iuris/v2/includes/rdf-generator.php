6e94e37cd4c6bc7ac4dfe71945eace9de2314f27
adrq <Adrian@Jupiter.local>
2015-08-04 13:46:09 -0400

    added download ability to rdf page
    -minor changes in rdf-generator

--- index-iuris/v2/includes/rdf-generator.php
+++ index-iuris/v2/includes/rdf-generator.php
@@ -1,15 +1,9 @@
 <?php
 /**
  * @file rdf-generator.php
- * Small summary of the current page's existence should go here.
+ * function generateRDF generates RDF formatted text given an object id
  */
 
-/**
- * Generates an RDF.
- *
- * @param {Array} $submission: Brief explanation of this variable goes here.
- * @return {String}
- */
 
 global $rolesRDFArray;
 
@@ -53,66 +47,18 @@ $rolesRDFArray = array(
 );
 
 
-
+/**
+ * Generates an RDF.
+ *
+ * @param {int} id of object to generate rdf from
+ * @return {String}
+ */
 
 function generateRDF($objectID) {
   global $mysqli;
-  $RDFRDF = array(
-  		"AUT" => "Author",
-  		"EDT" => "Editor",
-  		"PBL" => "Publisher",
-  		"TRL" => "Translator",
-  		"CRE" => "Creator",
-  		"ETR" => "Etcher",
-  		"EGR" => "Engraver",
-  		"OWN" => "Owner",
-  		"ART" => "Artist",
-  		"ARC" => "Architect",
-  		"BND" => "Binder",
-  		"BKD" => "Book designer",
-  		"BKP" => "Book producer",
-  		"CLL" => "Calligrapher",
-  		"CTG" => "Cartographer",
-  		"COL" => "Collector",
-  		"CLR" => "Colorist",
-  		"CWT" => "Commentator for written text",
-  		"COM" => "Compiler",
-  		"CMT" => "Compositor",
-  		"CRE" => "Creator",
-  		"DUB" => "Dubious author",
-  		"FAC" => "Facsimilist",
-  		"ILU" => "Illuminator",
-  		"ILL" => "Illustrator",
-  		"LTG" => "Lithographer",
-  		"PRT" => "Printer",
-  		"POP" => "Printer of plates",
-  		"PRM" => "Printmaker",
-  		"RPS" => "Repository",
-  		"RBR" => "Rubricator",
-  		"SCR" => "Scribe",
-  		"SCL" => "Sculptor",
-  		"TYD" => "Type designer",
-  		"TYG" => "Typographer",
-  		"ART" => "Visual Artist",
-  		"WDE" => "Wood engraver",
-  		"WDC" => "Wood cutter"
-  );
 	
   global $rolesRDFArray;
-  /*$rdfNamespaces = array(
-    "archive" => "collex:archive",
-    "title" => "dc:title",
-    "type" => "dc:type",
-    "genre" => "collex:genre",
-    "origin" => "origin",
-    "provenance" => "placeOfComposition",
-    "shelfmark" => "shelfmark",
-    "alternative-title" => "dcterms:alternative",
-    "source" => "collex:source",
-    "text-divisions" => "textDivisions",
-    "notes" => "notes",
-    "file-format", "fileFormat"
-  );*/
+
 
   $statement = $mysqli->prepare("SELECT custom_namespace, rdf_about, archive, title, type, url, origin, provenance, place_of_composition, shelfmark, freeculture, full_text_url, full_text_plain, is_full_text, image_url, source, metadata_xml_url, metadata_html_url, text_divisions, language, ocr, thumbnail_url, notes, file_format, date_created, date_updated, user_id FROM objects WHERE id = ? LIMIT 1");
   $statement->bind_param("s", $objectID);
@@ -120,6 +66,10 @@ function generateRDF($objectID) {
   $statement->store_result();
   
   $statement->bind_result($custom_namespace, $rdf_about, $archive, $title, $type, $url, $origin, $provenance, $place_of_composition, $shelfmark, $freeculture, $full_text_url, $full_text_plain, $is_full_text, $image_url, $source, $metadata_xml_url, $metadata_html_url, $text_divisions, $language, $ocr, $thumbnail_url, $notes, $file_format, $date_created, $date_updated, $user_id);
+  if ($statement->num_rows != 1){
+  	return "RECORD \"$objectID\" DOES NOT EXIST";
+  }
+  
   $statement->fetch();
 
   $rdf = "";
@@ -128,91 +78,91 @@ function generateRDF($objectID) {
   	return;  			
   }
   
-  $rdf .= file_get_contents("includes/rdf-header.rdf") . "\n<". $custom_namespace . " rdf:about=\"" . $rdf_about. "\">\n";
+  $rdf .= file_get_contents("includes/rdf-header.rdf") . "\n\t<". $custom_namespace . " rdf:about=\"" . $rdf_about. "\">\n\t";
   //TODO: add namespace identifier and uri once namespaces have been defined
-  $rdf .= "<rdfs:seeAlso rdf:resource=\"" . $url. "\"/>\n<collex:federation>INDEXIURIS</collex:federation>\n";
+  $rdf .= "<rdfs:seeAlso rdf:resource=\"" . $url. "\"/>\n\t<collex:federation>INDEXIURIS</collex:federation>\n";
 
   
   if ($archive!=""){
-  	$rdf .= "<collex:archive>".$archive."</collex:archive>\n";
+  	$rdf .= "\t<collex:archive>".$archive."</collex:archive>\n";
   }
 
   if ($title!=""){
-  	$rdf .= "<dc:title>".$title."</dc:title>\n";
+  	$rdf .= "\t<dc:title>".$title."</dc:title>\n";
   }
 
   if ($type!=""){
-  	$rdf .= "<ii:type>".$type."</ii:type>\n";
+  	$rdf .= "\t<ii:type>".$type."</ii:type>\n";
   }
 
   if ($origin!=""){
-  	$rdf .= "<ii:origin>".$origin."</ii:origin>\n";
+  	$rdf .= "\t<ii:origin>".$origin."</ii:origin>\n";
   }
 
   if ($provenance!=""){
-  	$rdf .= "<ii:provenance>".$provenance."</ii:provenance>\n";
+  	$rdf .= "\t<ii:provenance>".$provenance."</ii:provenance>\n";
   }
 
   //place of composition TODO: check namespace name with Colin and Abigail
   if($place_of_composition!=""){
-  	$rdf .= "<ii:composition>".$place_of_composition."</ii:composition>\n";
+  	$rdf .= "\t<ii:composition>".$place_of_composition."</ii:composition>\n";
   }
   
   if ($shelfmark!=""){
-  	$rdf .= "<ii:shelfmark>".$shelfmark."</ii:shelfmark>\n";
+  	$rdf .= "\t<ii:shelfmark>".$shelfmark."</ii:shelfmark>\n";
   }
   
   if ($freeculture!=""){
-  	$rdf .= "<collex:freeculture>".$freeculture."</collex:freeculture>\n";
+  	$rdf .= "\t<collex:freeculture>".$freeculture."</collex:freeculture>\n";
   }  
   
   //fulltext
   if ($is_full_text == "true"){
   	if ($full_text_url != ""){
-  		$rdf .= "<collex:text rdf:resource=\">".$full_text_url."\"/>\n";
+  		$rdf .= "\t<collex:text rdf:resource=\">".$full_text_url."\"/>\n";
   	}
   	else {
-  		$rdf .= "<collex:text>".$full_text_plain."</collex:text>\n";
+  		$rdf .= "\t<collex:text>".$full_text_plain."</collex:text>\n";
   	}
   }
 
   if ($image_url!=""){
-  	$rdf .= "<collex:image rdf:resource=\"".$image_url."\"/>\n";
+  	$rdf .= "\t<collex:image rdf:resource=\"".$image_url."\"/>\n";
   }
   if($source!=""){
-  	$rdf .= "<dc:source>".$source."</dc:title>\n";
+  	$rdf .= "\t<dc:source>".$source."</dc:title>\n";
   }
 
   if($metadata_xml_url!=""){
-  	$rdf .= "<collex:source_xml>".$metadata_xml_url."</collex:source_xml>\n";
+  	$rdf .= "\t<collex:source_xml>".$metadata_xml_url."</collex:source_xml>\n";
   }
 
   if($metadata_html_url!=""){
-  	$rdf .= "<collex:source_html>".$metadata_html_url."</collex:source_html>\n";
+  	$rdf .= "\t<collex:source_html>".$metadata_html_url."</collex:source_html>\n";
   }
 
   if($text_divisions!=""){
-  	$rdf .= "<ii:divisions>".$text_divisions."</ii:divisions>\n";
+  	$rdf .= "\t<ii:divisions>".$text_divisions."</ii:divisions>\n";
   }
 
   if($language!=""){
-  	$rdf .= "<dc:language>".$language."</dc:language>\n";
+  	$rdf .= "\t<dc:language>".$language."</dc:language>\n";
   }
 
   if($ocr!=""){
-  	$rdf .= "<collex:ocr>".$ocr."</collex:ocr>\n";
+  	$rdf .= "\t<collex:ocr>".$ocr."</collex:ocr>\n";
   }
 
   if($thumbnail_url!=""){
-  	$rdf .= "<<collex:thumbnail rdf:resource=\"".$thumbnail_url."\"/>\n";
+  	$rdf .= "\t<collex:thumbnail rdf:resource=\"".$thumbnail_url."\"/>\n";
   }
 
   if($notes!=""){
-  	$rdf .= "<ii:notes>".$notes."</ii:notes>\n";
+  	$rdf .= "\t<ii:notes>".$notes."</ii:notes>\n";
   }
 
   if($file_format!=""){
-  	$rdf .= "<ii:format>".$file_format."</ii:format>\n";
+  	$rdf .= "\t<ii:format>".$file_format."</ii:format>\n";
   }
   
   //role
@@ -223,7 +173,7 @@ function generateRDF($objectID) {
   $temp->bind_result($role, $value);
   
   while ($temp->fetch()){
-  	$rdf .= "<role:".$rolesRDFArray[$role].">".$value."</role:".$rolesRDFArray[$role].">\n";
+  	$rdf .= "\t<role:".$rolesRDFArray[$role].">".$value."</role:".$rolesRDFArray[$role].">\n";
   }
 
   
@@ -235,7 +185,7 @@ function generateRDF($objectID) {
   $temp->bind_result($genre);
   
   while ($temp->fetch()){
-  	$rdf .= "<ii:genre>".$genre."</ii:genre>\n";
+  	$rdf .= "\t<ii:genre>".$genre."</ii:genre>\n";
   }
   
   //alternative titles
@@ -246,7 +196,7 @@ function generateRDF($objectID) {
   $temp->bind_result($altTitle);
   
   while ($temp->fetch()){
-  	$rdf .= "<dcterms:alternative>".$altTitle."</dcterms:alternative>\n";
+  	$rdf .= "\t<dcterms:alternative>".$altTitle."</dcterms:alternative>\n";
   }
   
   //parts
@@ -264,7 +214,7 @@ function generateRDF($objectID) {
   	$part->bind_result($part_rdf_about);
   	$part->fetch();
   	
-  	$rdf .= "<dcterms:$type rdf:resource=\"".$part_rdf_about."\"/>\n";
+  	$rdf .= "\t<dcterms:$type rdf:resource=\"".$part_rdf_about."\"/>\n";
   }
   
   //date
@@ -275,11 +225,11 @@ function generateRDF($objectID) {
   $temp->bind_result($type, $machine, $human);
   
   while ($temp->fetch()){
-  	$rdf .= "<dc:date>\n<collex:date>\n<rdfs:label>".$human."</rdfs:label>\n<rdf:value>".$machine."<rdf:value>\n</collex:date>\n</dc:date>\n";
+  	$rdf .= "\t<dc:date>\n\t  <collex:date>\n\t    <rdfs:label>".$human."</rdfs:label>\n\t    <rdf:value>".$machine."<rdf:value>\n\t  </collex:date>\n\t</dc:date>\n";
   }
   
 
-  $rdf .= "</" . $custom_namespace . ">\n</rdf:RDF>\n";
+  $rdf .= "\t</" . $custom_namespace . ">\n</rdf:RDF>\n";
 
   return $rdf;
 }
--- index-iuris/v2/rdf.php
+++ index-iuris/v2/rdf.php
@@ -3,10 +3,22 @@
  * @file rdf.php
  * Prints rdf for a given record
  */
+
+require "includes/rdf-generator.php";
+require_once "includes/config.php";
+if (isset($_GET["id"])) :
+  $id = $_GET["id"];
+  if (isset($_GET["download"]) && $_GET["download"]=="true"){ //download the rdf file instead of displaying it
+	header("Content-Description: File Transfer");
+	header("Content-Disposition: attachment; filename=$id.rdf");
+    print generateRDF($_GET["id"]);
+    exit();
+  }
+
+
 $title = "View RDF";
 $loginRequired = true;
 require "includes/header.php";
-require "includes/rdf-generator.php";
 ?>
 
 
@@ -14,21 +26,27 @@ require "includes/rdf-generator.php";
   <div class="row">
     <div class="col-xs-12">
       <h1 class="pull-left">View RDF</h1>
+      <h3 class="pull-right"><a href="view?id=<?php print $id?>">Return to submission</a></h3>
     </div>
     <div class="col-xs-12">
-      
-      <?php if (isset($_GET["id"])) :?>
-      	<pre>
-      	  <?php print htmlspecialchars(generateRDF($_GET["id"]));?>
-      	</pre>
-      <?php else:?>
-        <p>Invalid ID. <a href="submissions">View submissions</a>
-      <?php endif?>
-      
+      <pre>
+        <?php print htmlspecialchars(generateRDF($id));?>
+      </pre>      
     </div>
   </div>
 </div>
-
+<?php else:?>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <h1 class="pull-left">View RDF</h1>
+    </div>
+    <div class="col-xs-12">
+      <p>Invalid ID. <a href="submissions">View submissions</a>
+    </div>
+  </div>
+</div>
+<?php endif?>
 
 
 
