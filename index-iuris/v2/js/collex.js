bbe2361cab089c6c074faf81d380d0f3656eb8dc
Collin Haines <collinhaines@me.com>
2015-08-07 13:48:28 -0400

    Cleanup and Layout Change
    - Removed reply and view-reply since they're not being utilized.
    - URL is annoying and renders improperly on the submissions table, so
    that's gone.
    - Object ID in submissions table was moved to hide below View/Edit/RDF
    for a hidden sort functionality.

--- index-iuris/v2/js/collex.js
+++ index-iuris/v2/js/collex.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
   $("[data-toggle='tooltip']").tooltip();
 
   // Initialize all DataTables.
-  $(".dt").dataTable();
+ $(".dt").dataTable();
 });
 
 /**
@@ -510,3 +510,4 @@ function editSuccess() {
     $("#editSuccess").fadeOut();
   }, 1500);
 }
+
--- index-iuris/v2/submissions.php
+++ index-iuris/v2/submissions.php
@@ -24,14 +24,12 @@ require "includes/header.php";
         <thead>
           <tr>
             <th>Title</th>
-            <th>URL</th>
             <th>Archive</th>
             <th>Type</th>
-            <th>Object ID</th>
             <?php if (isSuper()): ?>
               <th>User</th>
             <?php endif; ?>
-            <th></th>
+            <th>Tools</th>
           </tr>
         </thead>
         <tbody>
@@ -40,9 +38,9 @@ require "includes/header.php";
           $statement;
 
           if (isSuper()) {
-            $statement = $mysqli->prepare("SELECT id, url, title, archive, type, user_id FROM objects");
+            $statement = $mysqli->prepare("SELECT id, title, archive, type, user_id FROM objects");
           } else {
-            $statement = $mysqli->prepare("SELECT id, url, title, archive, type FROM objects WHERE user_id = ?");
+            $statement = $mysqli->prepare("SELECT id, title, archive, type FROM objects WHERE user_id = ?");
             $statement->bind_param("i", $_SESION["user_id"]);
           }
 
@@ -50,25 +48,24 @@ require "includes/header.php";
           $statement->store_result();
 
           if (isSuper()) {
-            $statement->bind_result($id, $url, $title, $archive, $type, $userID);
+            $statement->bind_result($id, $title, $archive, $type, $userID);
           } else {
-            $statement->bind_result($id, $url, $title, $archive, $type);
+            $statement->bind_result($id, $title, $archive, $type);
           }
           ?>
           <?php while ($statement->fetch()): ?>
             <tr>
               <td><?php print $title; ?></td>
-              <td><?php print $url; ?></td>
               <td><?php print $archive; ?></td>
               <td><?php print $type; ?></td>
-              <td><?php print $id; ?></td>
               <?php if (isSuper()): ?>
                 <td><?php print findUsername($userID); ?></td>
               <?php endif; ?>
-              <td class="text-center">
+              <td>
                 <a href="view?id=<?php print $id; ?>" class="btn btn-sm btn-primary">View</a>
                 <a href="edit?id=<?php print $id; ?>" class="btn btn-sm btn-success">Edit</a>
                 <a href="rdf?id=<?php print $id; ?>" class="btn btn-sm btn-default">RDF</a>
+                <span class="hide"><?php print $id; ?></span>
               </td>
             </tr>
           <?php endwhile; ?>
