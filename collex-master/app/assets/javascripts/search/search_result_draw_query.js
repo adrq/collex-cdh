36ae54646bb7d8b9e458803ee1e9edd0aaa77afd
adrq <adrian@Mars.local>
2015-11-17 11:10:35 -0500

    fixed missing image in search results page

--- collex-master/app/assets/javascripts/search/search_result_draw_query.js
+++ collex-master/app/assets/javascripts/search/search_result_draw_query.js
@@ -48,7 +48,7 @@ jQuery(document).ready(function($) {
 	}
 
 	function searchRemove(key, value) {
-		return window.pss.createHtmlTag("button", {'class': "trash select-facet", 'data-key': key, 'data-value': value, 'data-action': 'remove' }, '<img alt="Remove Term" src="'+window.site_path+'/assets/images/lvl2_trash.gif">' );
+		return window.pss.createHtmlTag("button", {'class': "trash select-facet", 'data-key': key, 'data-value': value, 'data-action': 'remove' }, '<img alt="Remove Term" src="'+window.site_path+'/assets/indexiuris/lvl2_trash.gif">' );
 	}
 
 	function getDisplayedKeyFromRole(role) {
